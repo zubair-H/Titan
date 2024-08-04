77b18ed36dc6a13a972a0746bbc425d3a1ebc38b
zubair-H <zubair.linkdin@gmail.com>
2024-08-04 11:37:11 -0400

    testing the frontend

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -9,12 +9,16 @@ import CheckBackendButton from './CheckBackendButton'
 export default function Home() {
     return (
         <div>
-            <CheckBackendButton/>
-            {/* <Navbar></Navbar>
+            <Navbar></Navbar>
+
             <Section1></Section1>
             <Section2></Section2>
-            <Section3></Section3> */}
-            
+            <Section3></Section3>
+
+            {/*
+              <CheckBackendButton/>
+            */}
+
 
 
         </div>
